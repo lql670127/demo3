dd151542f7e3e2fd7ba5ecbf441daace62a8e9f3
Liuqingling <1293759197@qq.com>
2023-05-03 19:07:36 +0800

    demo commit

--- src/main/java/com/example/demo2/mysql/User.java
+++ src/main/java/com/example/demo2/mysql/User.java
@@ -13,12 +13,4 @@ public class User {
     private int age;
     private String sex;
     private String address;
-
-    //    测试
-    public static void main(String[] args) {
-        System.out.println("hello git!");
-        System.out.println("hello git2!");
-        System.out.println("hello git3!");
-        System.out.println("hello git4!");
-    }
 }
